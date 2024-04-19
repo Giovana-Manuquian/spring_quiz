6ee70c41a52244f53abb922fe9ef035a3965eb36
Giovana-Manuquian <gmanuquian@gmail.com>
2024-04-18 22:00:53 -0300

    quiz

---
+++ src/main/java/com/quiz/quizapp/controller/QuestionController.java
@@ -0,0 +1,30 @@
+package com.quiz.quizapp.controller;
+
+import com.quiz.quizapp.Question;
+import com.quiz.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+}
